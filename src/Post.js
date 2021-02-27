c55d9048bc4719b272d56e6610e392796c86ef1e
Poniks <nadstogakuba@gmail.com>
2021-02-27 16:07:27 +0100

    changed parseInt on parseFloat, new sum per %

--- src/Post.js
+++ src/Post.js
@@ -28,7 +28,11 @@ const Post = () => {
         city: '',
         dateSell: `${year}-${newMonth}-${newDay}`,
         dateIssue: `${year}-${newMonth}-${newDay}`,
-        inputList: [['', 'usł.', '1', '', '0.00', '23%', '0.00', 0]],
+        inputList: [['', 'usł.', '1', '0.00', '0.00', '23%', '0.00', '0.00']],
+        inputListRecountVat: [['','23%','','']],
+        sumNet: '0.00',
+        sumVat: '0.00',
+        sumGross: '0.00',
     });
 
     const handleInputChange = (e, index, innerIndex) => {
@@ -36,21 +40,34 @@ const Post = () => {
         const list = [...state.inputList];
         list[index][innerIndex] = value;
         setState({ ...state, inputList: list });
-      };
+    };
 
     const handleRemoveClick = (index) => {
         const list = [...state.inputList];
         list.splice(index, 1);
     
         setState({ ...state, inputList: list });
-      };
+    };
 
     const handleAddClick = () => {
         setState({
-          ...state,
-          inputList: [...state.inputList, ['', 'usł.', '1', '', '0.00', '23%', '0.00', 0]],
+            ...state,
+            inputList: [...state.inputList, ['', 'usł.', '1', '0.00', '0.00', '23%', '0.00', '0.00']],     
         });
-      };
+    };
+
+    const handleAddNewLine = (e) => {
+        let test = state.inputListRecountVat.map((line, i) => (
+            line[1].includes(e.target.value)
+        ))
+
+        if(!test.includes(true)){
+            setState({
+                ...state,
+                inputListRecountVat: [...state.inputListRecountVat, ['',e.target.value,'','']],        
+            });
+        }
+    }
 
     const handleChange = input => e => {
         setState({
@@ -69,6 +86,34 @@ const Post = () => {
                 postSubmitted: true,
             })
         }
+    }
+
+    const recountSum = () => {
+        let arrNet = [];
+        let arrVat = [];
+        let arrGross = [];
+
+        arrNet.push(state.inputList.map((input, i) => {
+            let number = parseFloat(input[3], 10);
+            return number;
+        }));
+
+        arrVat.push(state.inputList.map((input, i) => {
+            let number = parseFloat(input[6], 10);
+            return number;
+        }));
+
+        arrGross.push(state.inputList.map((input, i) => {
+            let number = parseFloat(input[7], 10);
+            return number;
+        }));
+
+        setState({
+            ...state,
+            sumNet: (arrNet[0].reduce((a, b) => a + b)).toFixed(2),
+            sumVat: (arrVat[0].reduce((a, b) => a + b)).toFixed(2),
+            sumGross: (arrGross[0].reduce((a, b) => a + b)).toFixed(2),
+        })
     }
 
     const documents = [
@@ -328,7 +373,22 @@ const Post = () => {
                                                     size="small"
                                                     autoComplete="off"
                                                     margin="dense"
-                                                    onChange={(e) => handleInputChange(e, i, 3)}
+                                                    onChange={(e) => {
+                                                        handleInputChange(e, i, 3);
+                                                        const list = [...state.inputList];
+                                                        const vat = parseFloat(input[5],10)/100*e.target.value;
+                                                        const brutto = parseFloat(e.target.value) + vat;
+                                                        list[i][6] = (vat).toFixed(2);
+                                                        list[i][4] = (parseFloat(e.target.value,10)).toFixed(2);
+                                                        list[i][7] = (brutto).toFixed(2);
+                                                        setState({ ...state, inputList: list });                                                                 
+                                                    }}
+                                                    onBlur={(e) => {
+                                                        const list = [...state.inputList];
+                                                        list[i][3] = (parseFloat(e.target.value,10)).toFixed(2);
+                                                        setState({ ...state, inputList: list });
+                                                        recountSum(); 
+                                                    }}
                                                     className="price"
                                                 />
                                             </TableCell>
@@ -352,7 +412,17 @@ const Post = () => {
                                                     value={input[5]}
                                                     variant="outlined"
                                                     size="small"
-                                                    onChange={(e) => handleInputChange(e, i, 5)}
+                                                    onChange={(e) => {
+                                                        handleInputChange(e, i, 5);
+                                                        const list = [...state.inputList];
+                                                        const vat = parseFloat(input[5],10)/100*input[3];
+                                                        const brutto = parseFloat(input[3]) + vat;
+                                                        list[i][6] = (vat).toFixed(2);
+                                                        list[i][7] = (brutto).toFixed(2);
+                                                        setState({ ...state, inputList: list });
+                                                        recountSum();
+                                                        handleAddNewLine(e);
+                                                    }}
                                                     className="vat-rate"
                                                 >
                                                     {vat.map((option) => (
@@ -383,14 +453,21 @@ const Post = () => {
                                                     size="small"
                                                     autoComplete="off"
                                                     margin="dense"
-                                                    type="number"
+                                                    // type="number"
                                                     onChange={(e) => {
                                                         handleInputChange(e, i, 7);
                                                         const list = [...state.inputList];
-                                                        const vat = (input[7]/(100+parseInt(input[5],10)))*parseInt(input[5],10);
+                                                        const vat = (input[7]/(100+parseFloat(input[5],10)))*parseFloat(input[5],10);
                                                         list[i][6] = (vat).toFixed(2);
-                                                        list[i][4] = (input[7] - vat).toFixed(2)
+                                                        list[i][4] = (input[7] - vat).toFixed(2);
+                                                        list[i][3] = (input[7] - vat).toFixed(2);
                                                         setState({ ...state, inputList: list });
+                                                    }}
+                                                    onBlur={(e) => {
+                                                        const list = [...state.inputList];
+                                                        list[i][7] = (parseFloat(e.target.value,10)).toFixed(2);
+                                                        setState({ ...state, inputList: list });
+                                                        recountSum();
                                                     }}
                                                     className="price"
                                                 />
@@ -408,21 +485,27 @@ const Post = () => {
                                                 <AddIcon />
                                             </IconButton>
                                         </Button>
-                                        <TableCell rowSpan={2} colspan={3}/>
+                                        {/* {state.inputListRecountVat.forEach((input, i) => (
+                                            <TableCell rowSpan={2} colspan={3}/>
+                                        ))}  */}
+                                        <TableCell rowSpan={1 + state.inputListRecountVat.length} colspan={3}/> 
                                         <TableCell>Razem</TableCell>
-                                        <TableCell>3000</TableCell>
+                                        <TableCell>{state.sumNet}</TableCell>
                                         <TableCell></TableCell>
-                                        <TableCell>1000</TableCell>
-                                        <TableCell>4000</TableCell>
+                                        <TableCell>{state.sumVat}</TableCell>
+                                        <TableCell>{state.sumGross}</TableCell>
                                     </TableRow>
-                                    <TableRow>
-                                        <TableCell></TableCell>
-                                        <TableCell>W tym</TableCell>
-                                        <TableCell>3000%</TableCell>
-                                        <TableCell>23%</TableCell>
-                                        <TableCell>1000</TableCell>
-                                        <TableCell>4000</TableCell>
-                                    </TableRow>
+                                    {state.inputListRecountVat.map((input,i) => (
+                                        <TableRow row key={i}>
+                                            <TableCell></TableCell>
+                                            <TableCell>W tym</TableCell>
+                                            <TableCell>3000%</TableCell>
+                                            <TableCell>{input[1]}</TableCell>
+                                            <TableCell>1000</TableCell>
+                                            <TableCell>4000</TableCell>
+                                        </TableRow>
+                                    ))}
+                                    
                                 </TableBody>
                             </Table>
                         </TableContainer>
